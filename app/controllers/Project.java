0367497b46b892fcf7434a5cbc6882700423e643
Murthy10 <samuel.kurath@gmail.com>
2015-03-27 19:22:44 +0100

    Bug with Form.form solved and started with Controller

--- app/controllers/Project.java
+++ app/controllers/Project.java
@@ -1,25 +1,91 @@
 package controllers;
 
-import play.data.DynamicForm;
-import play.data.Form;
+
+import play.data.validation.Constraints;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.helper.form;
 import views.html.newProject;
+import play.data.Form;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import patch.PatchedForm;
+
+
 
 public class Project extends Controller {
-        public static Result addProject() {
- /*           DynamicForm requestData = Form.form().bindFromRequest();
-            String firstname = requestData.get("firstname");
-            String lastname = requestData.get("lastname");
-            return ok("Hello " + firstname + " " + lastname);
 
-*/
-        models.Project project = Form.form(models.Project.class).bindFromRequest().get();
-        project.save();
-        return redirect(routes.Application.index());
+    public static class NewProject {
+        @Constraints.Required
+        public String title;
+        @Constraints.Required
+        public String description;
+        @Constraints.Required
+        public String contact;
+        @Constraints.Required
+        public String endsAt;
+        @Constraints.Required
+        public String startsAt;
+
+        public String validate() {
+            if (title.isEmpty()) {
+                return "Title is empty";
+            }else if(description.isEmpty()){
+                return "Description is empty";
+            }else if(contact.isEmpty()){
+                return "Contact is empty";
+            }else if(endsAt.isEmpty()){
+                return "endsAt is empty";
+            }else if(startsAt.isEmpty()){
+                return "startsAt is empty";
+            }
+            return null;
+        }
+    }
+
+    //TODO: https://gist.github.com/ndeverge/3074629
+    public static Result addProject() throws Exception{
+        //Form<NewProject> projectForm = Form.form(NewProject.class).bindFromRequest();
+        Form<NewProject> projectForm = new PatchedForm<NewProject>(NewProject.class).bindFromRequest();
+        try{
+            System.out.println(projectForm.get().title);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        if (projectForm.hasErrors()) {
+            return badRequest(newProject.render(projectForm));
+        } else {
+            String title = projectForm.get().title;
+            System.out.println("title " + title);
+            String description = projectForm.get().description;
+            java.sql.Date endsAt = stringToSqlDate(projectForm.get().endsAt);
+            java.sql.Date startsAt = stringToSqlDate(projectForm.get().startsAt);
+            String contact = projectForm.get().contact;
+            models.Project project = new models.Project();
+            project.setTitle(title);
+            project.setDescription(description);
+            project.setEndsAt(endsAt);
+            project.setStartsAt(startsAt);
+            project.setContact(contact);
+            project.save();
+            return redirect(routes.Application.index());
+        }
     }
 
     public static Result foo() {
-        return ok(newProject.render());
+        return ok(newProject.render(Form.form(NewProject.class)));
+    }
+
+    private static java.sql.Date stringToSqlDate(String date)throws Exception{
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date dateUtil;
+        try {
+            dateUtil = sdf1.parse(date);
+        }catch (  ParseException ex) {
+            throw new Exception("stringToSqlDate()",ex);
+        }
+        java.sql.Date sqlStartDate = new Date(dateUtil.getTime());
+        return sqlStartDate;
     }
 }
