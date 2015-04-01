07a98c4b5752701284a888c8bfb1387da493dc89
Manuel Roth <manuelroth@hotmail.ch>
2015-04-01 22:17:49 +0200

    Added Minus Button for removeing rows

--- public/js/newProject.js
+++ public/js/newProject.js
@@ -1,6 +1,8 @@
 $(function() {
     $('#addGood').click(function(){
-        console.log("newProject.js loaded");
-        $('#main-table tbody').append($("#main-table tbody tr:last").clone());
+        $('#main-table tbody').append('<tr><td class="product-image"><input class="form-control" id="name" type="number" name="amount" value=""></td><td class="product-title"><input class="form-control" id="name" type="text" name="donation" value=""></td></tr>');
+    });
+    $('#deleteGood').click(function(){
+        $("#main-table tbody tr:last").detach();
     });
 });
