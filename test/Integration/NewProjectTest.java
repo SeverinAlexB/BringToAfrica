d21a13d88e2919b4df7ac2d68fb648d0f53d77ae
Severin Bühler <sevi_buehler@hotmail.com>
2015-04-01 21:00:10 +0200

    Changes for Integrationtests

--- test/Integration/NewProjectTest.java
+++ test/Integration/NewProjectTest.java
@@ -1,8 +1,12 @@
 package Integration;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import models.Project;
 import org.junit.Test;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import play.db.ebean.Model;
 import play.libs.F;
 import play.test.TestBrowser;
 import play.mvc.*;
@@ -17,14 +21,79 @@ import static org.fluentlenium.core.filter.FilterConstructor.*;
 import static play.test.Helpers.running;
 
 public class NewProjectTest {
+    private boolean projektdatenCalled = false;;
+    public void gotoProjektdaten(TestBrowser browser) {
+        browser.goTo("http://localhost:3333/projects/new");
+        projektdatenCalled = true;
+    }
+    private boolean warenCalled = false;
+    public void gotoWaren(TestBrowser browser) {
+        assert projektdatenCalled;
+        browser.$("[name^='title']").text("Help Children get clothes");
+        browser.$("[name^='description']").text("Help Children get clothes");
+        browser.$("[name^='startsAt']").text("2015-03-10");
+        browser.$("[name^='endsAt']").text("2015-04-29");
+        browser.$("#btnContinue1").click();
+        warenCalled = true;
+    }
+    private boolean kontaktCalled = false;
+    public void gotoKontakt(TestBrowser browser) {
+        assert warenCalled;
+        browser.$("[name^='amount']").text("10");
+        browser.$("[name^='donation']").text("Schuhe");
+        browser.$("#btnContinue2").click();
+        kontaktCalled = true;
+    }
+    private boolean bestaetigungCalled = false;
+    public void gotoBestaetigung(TestBrowser browser) {
+        assert kontaktCalled;
+        browser.$("[name^='country']").text("Nigeria");
+        browser.$("[name^='city']").text("New York");
+        browser.$("[name^='street']").text("Walenseestrasse 5");
+        browser.$("[name^='contact']").text("Claudia Hofstetter\nLehstrasse 5\n 8000 Zürich");
+        browser.$("#btnContinue3").click();
+        bestaetigungCalled = true;
+    }
 
     @Test
-    public void test2() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new F.Callback<TestBrowser>() {
+    public void straightThroughTest() {
+        running(testServer(3333, fakeApplication()), new HtmlUnitDriver(BrowserVersion.CHROME), new F.Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333/projects/new");
+                gotoProjektdaten(browser);
+                assertThat(!browser.pageSource().contains("error"));
+                gotoWaren(browser);
+                assertThat(!browser.pageSource().contains("error"));
+                gotoKontakt(browser);
+                assertThat(!browser.pageSource().contains("error"));
+                gotoBestaetigung(browser);
+                assertThat(!browser.pageSource().contains("error"));
+                //browser.$("#btnProjektErstellen").click();
+            }
+        });
+    }
+    @Test
+    public void projektDatenInputValidTest() {
+        running(testServer(3333, fakeApplication()), new HtmlUnitDriver(BrowserVersion.CHROME), new F.Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                gotoProjektdaten(browser);
+                browser.$("#btnContinue1").click();
+                assertThat(browser.pageSource().contains("error"));
 
-              //  assertThat(browser.pageSource()).contains("Add Project");
+                browser.$("[name^='title']").text("Help Children get clothes");
+                browser.$("#btnContinue1").click();
+                assertThat(browser.pageSource().contains("error"));
+
+                browser.$("[name^='description']").text("Help Children get clothes");
+                browser.$("#btnContinue1").click();
+                assertThat(browser.pageSource().contains("error"));
+
+                browser.$("[name^='startsAt']").text("2015-03-10");
+                browser.$("#btnContinue1").click();
+                assertThat(browser.pageSource().contains("error"));
+
+                browser.$("[name^='endsAt']").text("2015-04-29");
+                browser.$("#btnContinue1").click();
+                assertThat(!browser.pageSource().contains("error"));
             }
         });
     }
