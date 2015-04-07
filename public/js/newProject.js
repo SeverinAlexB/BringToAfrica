bb33dd29de82da99fcc9dfb9db5b1ae46fc1e73c
Murthy10 <samuel.kurath@gmail.com>
2015-04-07 18:45:14 +0200

    Fixed multible amounts input

--- public/js/newProject.js
+++ public/js/newProject.js
@@ -1,6 +1,9 @@
 $(function() {
+    var rows = document.getElementById("main-table").getElementsByTagName("tbody")[0].getElementsByTagName("tr").length.toString();
+    var amount = "amounts[" + rows + "]";
+    var donation = "donations[" + rows + "]";
     $('#addGood').click(function(){
-        $('#main-table tbody').append('<tr><td class="product-image"><input class="form-control" id="name" type="number" name="amountField" value=""></td><td class="product-title"><input class="form-control" id="name" type="text" name="donationField" value=""></td></tr>');
+        $('#main-table tbody').append('<tr><td class="product-image"><input class="form-control" id="name" type="number" name="' + amount + '" value=""></td><td class="product-title"><input class="form-control" id="name" type="text" name="' + donation + '" value=""></td></tr>');
     });
     $('#deleteGood').click(function(){
         $("#main-table tbody tr:last").detach();
