6e6452527c1f188e502c69e8af308597d85d5fdf
Manuel Roth <manuelroth@hotmail.ch>
2015-04-10 21:50:07 +0200

    Fixed tab change on button click

--- public/js/newProject.js
+++ public/js/newProject.js
@@ -1,11 +1,22 @@
 $(function() {
     $('#addGood').click(function(){
-        var rows = $('#main-table >tbody >tr').length.toString();
+        var rows = $('#main-table >tbody >tr').length;
         var amount = "amounts[" + rows + "]";
         var donation = "donations[" + rows + "]";
+        if(rows > 3) return;
         $('#main-table tbody').append('<tr><td class="product-image"><input class="form-control" id="name" type="number" name="' + amount + '" value=""></td><td class="product-title"><input class="form-control" id="name" type="text" name="' + donation + '" value=""></td></tr>');
     });
     $('#deleteGood').click(function(){
+    	var rows = $('#main-table >tbody >tr').length;
+    	if(rows < 2) return;
         $("#main-table tbody tr:last").detach();
     });
+
+    $('#btnContinue1').click(function(){
+  		$('.nav-tabs > .active').next('li').find('a').trigger('click');
+	});
+
+	$('#btnContinue2').click(function(){
+  		$('.nav-tabs > .active').next('li').find('a').trigger('click');
+	});
 });
