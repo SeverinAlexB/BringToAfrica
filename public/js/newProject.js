ba2657b9c5cd2483b1c4ee8e2a2eb2624c7a37a6
Manuel Roth <manuelroth@hotmail.ch>
2015-04-12 19:19:03 +0200

    Added html5 form validation for login, registration and newProject

--- public/js/newProject.js
+++ public/js/newProject.js
@@ -4,7 +4,7 @@ $(function() {
         var amount = "amounts[" + rows + "]";
         var donation = "donations[" + rows + "]";
         if(rows > 3) return;
-        $('#main-table tbody').append('<tr><td class="product-image"><input class="form-control" id="name" type="number" name="' + amount + '" value=""></td><td class="product-title"><input class="form-control" id="name" type="text" name="' + donation + '" value=""></td></tr>');
+        $('#main-table tbody').append('<tr><td class="product-image"><input class="form-control" id="name" type="number" name="' + amount + '" value="" required></td><td class="product-title"><input class="form-control" id="name" type="text" name="' + donation + '" value="" required></td></tr>');
     });
     $('#deleteGood').click(function(){
     	var rows = $('#main-table >tbody >tr').length;
