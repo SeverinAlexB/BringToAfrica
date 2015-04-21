63d9c4aa94b54caab6e9a20da380d7cca183ced1
Lukas Martinelli <me@lukasmartinelli.ch>
2015-04-21 17:39:57 +0200

    Reactored donate method

--- app/controllers/ProjectController.java
+++ app/controllers/ProjectController.java
@@ -57,30 +57,40 @@ public class ProjectController extends Controller {
             return badRequest(views.html.project.donate.render(project, form));
         } else {
             List<DonationGoal> goals = project.getDonationGoals();
-            ArrayList<DonationData> donations = new ArrayList<>();
-            for (int i = 0; i < form.get().amounts.size(); i++) {
-                String typeName = form.get().donations.get(i);
-                String amount = form.get().amounts.get(i);
-                DonationType type = DonationTypeService.getDonationTypeByName(typeName);
-
-                User user = ApplicationController.getCurrentUser();
-                DonationGoal goal = goals.stream()
-                        .filter(g -> g.getType().getName() == type.getName())
-                        .limit(2)
-                        .collect(Collectors.toList()).get(0);
-
-                Donation donation = new Donation(user, goal);
-                donation.setAmount(Integer.parseInt(amount));
-                donation.save();
-
-                donations.add(new DonationData(donation));
-            }
+            ArrayList<DonationData> donations = createDonations(form, goals);
 
             ProjectDonationData donation = new ProjectDonationData();
             donation.project = project;
             donation.donations = donations;
             return ok(views.html.project.donateSuccess.render(donation));
         }
+    }
+
+    private static ArrayList<DonationData> createDonations(Form<CreateDonationData> form, List<DonationGoal> goals) {
+        ArrayList<DonationData> donations = new ArrayList<>();
+
+        for (int i = 0; i < form.get().amounts.size(); i++) {
+            String typeName = form.get().donations.get(i);
+            int amount = Integer.parseInt(form.get().amounts.get(i));
+
+            User user = ApplicationController.getCurrentUser();
+            DonationGoal goal = getGoalByType(goals, typeName);
+
+            Donation donation = new Donation(user, goal);
+            donation.setAmount(amount);
+            donation.save();
+
+            donations.add(new DonationData(donation));
+        }
+        return donations;
+    }
+
+    private static DonationGoal getGoalByType(List<DonationGoal> goals, String typeName) {
+        DonationType type = DonationTypeService.getDonationTypeByName(typeName);
+        return goals.stream()
+                .filter(g -> g.getType().getName().equals(type.getName()))
+                .limit(2)
+                .collect(Collectors.toList()).get(0);
     }
 
     private static Form<CreateDonationData> createDonationForm(Project project) {
