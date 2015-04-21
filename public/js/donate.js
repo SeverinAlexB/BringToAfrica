33bce30a8db0df40800c0c7d62e377c2343d65e4
Lukas Martinelli <me@lukasmartinelli.ch>
2015-04-21 15:23:11 +0200

    Fixed javascript for changing donation value

--- public/js/donate.js
+++ public/js/donate.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
     $('.choose-pricing button').on('click', function () {
         var target = $(this).attr('data-target');
-        $(target).val($(this).text());
+        $(target).attr('value', $(this).text());
+
+    });
+    $('.choose-pricing input.form-control').change(function() {
+        var target = $(this).attr('data-target');
+        $(target).attr('value', $(this).val());
     });
 });
