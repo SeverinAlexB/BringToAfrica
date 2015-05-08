780e8de962e7782a8a042bf59a874bb4ea8c5406
Murthy10 <samuel.kurath@gmail.com>
2015-05-08 10:09:11 +0200

    newProject validation is okey now

--- public/js/newProject.js
+++ public/js/newProject.js
@@ -3,21 +3,10 @@ $(function() {
         $('.next').on('click', function () {
             var current = $(this).data('currentBlock'),
                 next = $(this).data('nextBlock');
-
-            // only validate going forward. If current group is invalid, do not go further
-            // .parsley().validate() returns validation result AND show errors
             if (next > current)
                 if (false === $('#newProjectForm').parsley().validate('block' + current))
                     return;
 
-            // validation was ok. We can go on next step.
-            //$('.block' + current)
-            //    .removeClass('show')
-            //    .addClass('hidden');
-
-            //$('.block' + next)
-            //    .removeClass('hidden')
-            //    .addClass('show');
             $('.nav-tabs > .active').next('li').find('a').trigger('click');
 
         });
@@ -28,7 +17,7 @@ $(function() {
         var amount = "amounts[" + rows + "]";
         var donation = "donations[" + rows + "]";
         if(rows > 3) return;
-        $('#main-table tbody').append('<tr><td class="product-image"><input class="form-control" id="name" data-parsley-group="block2" required type="number" name="' + amount + '" value=""></td><td class="product-title"><input class="form-control" id="name" data-parsley-group="block2" required type="text" name="' + donation + '" value=""></td></tr>');
+        $('#main-table tbody').append('<tr><td class="product-image"><input class="form-control" id="name" data-parsley-group="block2" required parsley-type="number" name="' + amount + '" value=""></td><td class="product-title"><input class="form-control" id="name" data-parsley-group="block2" required type="text" name="' + donation + '" value=""></td></tr>');
     });
     $('#deleteGood').click(function(){
     	var rows = $('#main-table >tbody >tr').length;
